d229a28b9cf256753a734e27ce8996ad413edfed
redy <redy.ru@gmail.com>
2016-02-17 22:22:46 +0800

    xxx

--- theme/src/scripts/functions.js
+++ theme/src/scripts/functions.js
@@ -5,4 +5,12 @@ $(document).ready(function($){
       gallery: {enabled:true},
     });
   }
+  if($('.share-modal').length > 0){
+    $('.share-modal').magnificPopup({
+      type: 'inline',
+      preloader: false,
+      focus: false,
+      modal: false
+    });
+  }
 });
